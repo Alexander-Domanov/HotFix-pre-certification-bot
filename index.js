e390ce2dec2972043113c7e29dcb1f1eed0044dd
Alexander-Domanov <alexanderdomanov.dev@gmail.com>
2024-08-06 22:39:26 +0300

    feat: error,start_keyboard,start_keyboard

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
 require('dotenv').config()
 
-const { Bot, GrammyError, HttpError } = require('grammy')
+const {Bot, Keyboard, InlineKeyboard, GrammyError, HttpError} = require('grammy')
 
 const bot = new Bot(process.env.BOT_API_KEY)
 
 bot.command('start', async (ctx) => {
-   await ctx.reply('Привет! Я - HotFix-pre-certification-bot👩‍🎓👨‍🎓 \n Я помогу тебе подготовиться к сертификации')
+    const startKeyboard = new Keyboard()
+        .text('HTML')
+        .text('CSS')
+        .row()
+        .text('JS')
+        .text('REACT')
+        .resized()
+
+    await ctx.reply('Привет! Я - HotFix-pre-certification-bot👩‍🎓👨‍🎓 \n Я помогу тебе подготовиться к сертификации')
+    await ctx.reply('С чего начнем? Выбери тему вопроса в меню 👇', {
+        reply_markup: startKeyboard
+    })
 })
 
-bot.hears('HTML', async (ctx)=> {
-   await ctx.reply('Что такое HTML?')
+bot.hears(['HTML','CSS','JS','REACT'], async (ctx) => {
+    const inlineKeyboard = new InlineKeyboard()
+        .text('Получить ответ', 'getAnswer')
+        .text('Отменить', 'cancel')
+    await ctx.reply(`Что такое ${ctx.message.text}?`, {
+        reply_markup: inlineKeyboard
+    })
 })
 
 bot.catch((err) => {
-   const ctx = err.ctx;
-   console.error(`Error while handling update ${ctx.update.update_id}:`);
-   const e = err.error;
-   if (e instanceof GrammyError) {
-      console.error("Error in request:", e.description);
-   } else if (e instanceof HttpError) {
-      console.error("Could not contact Telegram:", e);
-   } else {
-      console.error("Unknown error:", e);
-   }
+    const ctx = err.ctx;
+    console.error(`Error while handling update ${ctx.update.update_id}:`);
+    const e = err.error;
+    if (e instanceof GrammyError) {
+        console.error("Error in request:", e.description);
+    } else if (e instanceof HttpError) {
+        console.error("Could not contact Telegram:", e);
+    } else {
+        console.error("Unknown error:", e);
+    }
 });
 
 bot.start()
