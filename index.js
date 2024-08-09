aa1cb1b579a6839c390f1fe100b1a4850f410723
Alexander-Domanov <alexanderdomanov.dev@gmail.com>
2024-08-09 23:39:21 +0300

    ref: changing the logic

--- index.js
+++ index.js
@@ -7,26 +7,27 @@ const { getRandomQuestion, getCorrectAnswer } = require('./utils')
 
 bot.command('start', async (ctx) => {
     const startKeyboard = new Keyboard()
-        .text('HTML')
+        .text('classic')
         .text('CSS')
         .row()
-        .text('JS')
-        .text('REACT')
+        .text('JavaScript')
+        .text('React')
         .row()
         .text('Случайный вопрос')
-        .resized()
-
-    await ctx.reply('Привет! Я - HotFix-pre-certification-bot👩‍🎓👨‍🎓 \n Я помогу тебе подготовиться к сертификации')
+        .resized();
+    await ctx.reply(
+        'Привет! Я - Frontend Interview Prep Bot 🤖 \nЯ помогу тебе подготовиться к интервью по фронтенду',
+    );
     await ctx.reply('С чего начнем? Выбери тему вопроса в меню 👇', {
-        reply_markup: startKeyboard
-    })
-})
+        reply_markup: startKeyboard,
+    });
+});
 
 bot.hears(
-    ['HTML', 'CSS', 'JavaScript', 'React', 'Случайный вопрос'],
+    ['classic', 'CSS', 'JavaScript', 'React', 'Случайный вопрос'],
     async (ctx) => {
-        const topic = ctx.message.text.toLowerCase()
-        const { question, questionTopic } = getRandomQuestion(topic)
+        const topic = ctx.message.text.toLowerCase();
+        const { question, questionTopic } = getRandomQuestion(topic);
 
         let inlineKeyboard;
 
@@ -40,7 +41,7 @@ bot.hears(
                         questionId: question.id,
                     }),
                 ),
-            ])
+            ]);
 
             inlineKeyboard = InlineKeyboard.from(buttonRows);
         } else {
@@ -50,50 +51,60 @@ bot.hears(
                     type: questionTopic,
                     questionId: question.id,
                 }),
-            )
+            );
         }
 
         await ctx.reply(question.text, {
             reply_markup: inlineKeyboard,
-        })
+        });
     },
-
-)
+);
 
 bot.on('callback_query:data', async (ctx) => {
-    const callbackData = JSON.parse(ctx.callbackQuery.data)
+    const callbackData = JSON.parse(ctx.callbackQuery.data);
 
     if (!callbackData.type.includes('option')) {
-        const answer = getCorrectAnswer(callbackData.type, callbackData.questionId)
+        const answer = getCorrectAnswer(callbackData.type, callbackData.questionId);
         await ctx.reply(answer, {
             parse_mode: 'HTML',
             disable_web_page_preview: true,
-        })
-        await ctx.answerCallbackQuery()
-        return
+        });
+        await ctx.answerCallbackQuery();
+        return;
     }
 
     if (callbackData.isCorrect) {
-        await ctx.reply('Верно ✅')
+        await ctx.reply('Верно ✅');
         await ctx.answerCallbackQuery();
-        return
+        return;
     }
 
-    const answer = getCorrectAnswer(callbackData.type.split('-')[0], callbackData.questionId)
-    await ctx.reply(`Неверно ❌ Правильный ответ: ${answer}`)
-})
+    const answer = getCorrectAnswer(
+        callbackData.type.split('-')[0],
+        callbackData.questionId,
+    );
+    await ctx.reply(`Неверно ❌ Правильный ответ: ${answer}`);
+    await ctx.answerCallbackQuery();
+});
 
 bot.catch((err) => {
     const ctx = err.ctx;
     console.error(`Error while handling update ${ctx.update.update_id}:`);
     const e = err.error;
     if (e instanceof GrammyError) {
-        console.error("Error in request:", e.description);
+        console.error('Error in request:', e.description);
     } else if (e instanceof HttpError) {
-        console.error("Could not contact Telegram:", e);
+        console.error('Could not contact Telegram:', e);
     } else {
-        console.error("Unknown error:", e);
+        console.error('Unknown error:', e);
     }
 });
 
-bot.start()
+bot.start();
+
+// {
+//     "id": 6,
+//     "text": "Для чего используется атрибут 'placeholder'?",
+//     "hasOptions": false,
+//     "answer": "Атрибут 'placeholder' используется для отображения вводимого текста в поле формы до того, как пользователь начнет вводить свои данные. Это помогает предоставить подсказку или пример того, что должен ввести пользователь."
+// }
--- utils.js
+++ utils.js
@@ -1,8 +1,8 @@
-const questions = require('./question.json')
+const questions = require('./questions.json');
 const { Random } = require('random-js');
 
 const getRandomQuestion = (topic) => {
-    const random = new Random()
+    const random = new Random();
 
     let questionTopic = topic.toLowerCase();
 
@@ -10,9 +10,9 @@ const getRandomQuestion = (topic) => {
         questionTopic =
             Object.keys(questions)[
                 random.integer(0, Object.keys(questions).length - 1)
-                ]
+                ];
     }
-
+    console.log('topic', questions)
     const randomQuestionIndex = random.integer(
         0,
         questions[questionTopic].length - 1,
@@ -24,14 +24,14 @@ const getRandomQuestion = (topic) => {
     };
 };
 
-const getCorrectAnswer = (topic,id) => {
-    const question = questions[topic].find((question) => question.id === id )
+const getCorrectAnswer = (topic, id) => {
+    const question = questions[topic].find((question) => question.id === id);
 
     if (!question.hasOptions) {
-        return question.answer
+        return question.answer;
     }
 
-    return question.options.find((option)=> option.isCorrect ).text
-}
+    return question.options.find((option) => option.isCorrect).text;
+};
 
-module.exports = { getRandomQuestion, getCorrectAnswer }
+module.exports = { getRandomQuestion, getCorrectAnswer };
